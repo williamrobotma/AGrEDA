e112308cf0b3fec2486d51f0726a67a7af7a882c
William Ma <mawilliam.v20@gmail.com>
2023-07-09 02:19:32 -0400

    added pdac final, fixed up prep to be able to have ps of different seeds

---
+++ batch_scripts/run_all-pdac-FINAL.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -x
+
+mkdir -p logs/ADDA
+mkdir -p logs/CellDART
+mkdir -p logs/CORAL
+mkdir -p logs/DANN
+
+nohup ./batch_scripts/ADDA/run_adda-pdac-FINAL.sh 1> logs/ADDA/run-pdac-FINAL.out 2> logs/ADDA/run-pdac-FINAL.err &
+nohup ./batch_scripts/CellDART/run_CellDART-pdac-FINAL.sh 1> logs/CellDART/run-pdac-FINAL.out 2> logs/CellDART/run-pdac-FINAL.err &
+nohup ./batch_scripts/CORAL/run_coral-pdac-FINAL.sh 1> logs/CORAL/run-pdac-FINAL.out 2> logs/CORAL/run-pdac-FINAL.err &
+nohup ./batch_scripts/DANN/run_dann-pdac-FINAL.sh 1> logs/DANN/run-pdac-FINAL.out 2> logs/DANN/run-pdac-FINAL.err &
--- dann.py
+++ dann.py
@@ -60,9 +60,15 @@ parser.add_argument(
     default=None,
     help="seed to use for torch and numpy; overrides that in config file",
 )
+parser.add_argument(
+    "--ps_seed",
+    default=-1,
+    help="specific pseudospot seed to use; default of -1 corresponds to 623",
+)
 
 # %%
 args = parser.parse_args()
+PS_SEED = int(args.ps_seed)
 MODEL_DIR = args.model_dir
 SEED_OVERRIDE = args.seed_override
 
@@ -233,6 +239,7 @@ mat_sp_d, mat_sp_meta_d, st_sample_id_l = data_loading.load_spatial(
 # Load sc data
 sc_mix_d, lab_mix_d, sc_sub_dict, sc_sub_dict2 = data_loading.load_sc(
     selected_dir,
+    seed_int=PS_SEED,
     **data_params,
 )
 
