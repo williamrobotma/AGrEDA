4f3a52edb8073dc7af10a6ccc1c5fbdb5da35481
William Ma <mawilliam.v20@gmail.com>
2023-07-10 12:55:36 -0400

    made ht a little less ambitious

--- batch_scripts/sbatch-final_hyperval.sh
+++ batch_scripts/sbatch-final_hyperval.sh
@@ -9,9 +9,9 @@
 #SBATCH --output=logs/final_hyperval-%N-%j.out
 
 set -x
-source .venv/bin/activate
+source ~/.venv-agreda/bin/activate
 
-./hyperval_generated.py -n ADDA -cdir configs/generated_pdac -e > logs/ADDA/generated_pdac/final_hyperval.txt
-./hyperval_generated.py -n CellDART -cdir configs/generated_pdac -e > logs/CellDART/generated_pdac/final_hyperval.txt
-./hyperval_generated.py -n DANN -cdir configs/generated_pdac -e > logs/DANN/generated_pdac/final_hyperval.txt
-./hyperval_generated.py -n CORAL -cdir configs/generated_pdac > logs/CORAL/generated_pdac/final_hyperval.txt
+./hyperval_generated.py -n ADDA -cdir configs/generated_pdac --results_folder logs/ADDA/generated_pdac
+./hyperval_generated.py -n CellDART -cdir configs/generated_pdac --results_folder logs/CellDART/generated_pdac
+./hyperval_generated.py -n DANN -cdir configs/generated_pdac --results_folder logs/DANN/generated_pdac
+./hyperval_generated.py -n CORAL -cdir configs/generated_pdac --results_folder logs/CORAL/generated_pdac
--- batch_scripts/sbatch-gen-configs.sh
+++ batch_scripts/sbatch-gen-configs.sh
@@ -3,8 +3,8 @@
 #SBATCH --account=rrg-aminemad
 
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=8G
-#SBATCH --time=00:15:00
+#SBATCH --mem=4G
+#SBATCH --time=0005:00
 
 #SBATCH --output=logs/prep-genconfigs-%N-%j.out
 
@@ -19,11 +19,10 @@ set -x
 # pip install --no-index --upgrade pip
 # pip install --no-index -r requirements_cc.txt
 # ./gen_venv_cc.sh
-source ~/scratch/AGrEDA/.venv/bin/activate
-
+source ~/.venv-agreda/bin/activate
 # if ["$SLURM_ARRAY_TASK_ID" == "5"]; then
 
-jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --inplace --execute gen_configs_pdac_adda.ipynb
-jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --inplace --execute gen_configs_pdac_celldart.ipynb
+# jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --inplace --execute gen_configs_pdac_adda.ipynb
+# jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --inplace --execute gen_configs_pdac_celldart.ipynb
 jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --inplace --execute gen_configs_pdac_coral.ipynb
-jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --inplace --execute gen_configs_pdac_dann.ipynb
+# jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --inplace --execute gen_configs_pdac_dann.ipynb
