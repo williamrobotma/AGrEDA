8b95b32bc3ef084e32789ae98646c46df539b002
William Ma <mawilliam.v20@gmail.com>
2023-07-12 18:17:47 -0400

    excluded some samples from dlpfc to be used as test

--- batch_scripts/run-prep-celldart.sh
+++ batch_scripts/run-prep-celldart.sh
@@ -5,38 +5,38 @@ set -x
 ps_seeds=(3679 343 25 234 98098)
 
 # original paper
-# for i in "${!ps_seeds[@]}"; do
-#     ps_seed=${ps_seeds[$i]}
+for i in "${!ps_seeds[@]}"; do
+    ps_seed=${ps_seeds[$i]}
     
-#     echo ps_seed: $ps_seed
-#     ./prep_data.py -s minmax \
-#         --dset dlpfc \
-#         --st_id spatialLIBD \
-#         --sc_id GSE144136 \
-#         --nmarkers 20 \
-#         --nmix 8 \
-#         --samp_split \
-#         --ps_seed=$ps_seed
+    echo ps_seed: $ps_seed
+    ./prep_data.py -s minmax \
+        --dset dlpfc \
+        --st_id spatialLIBD \
+        --sc_id GSE144136 \
+        --nmarkers 20 \
+        --nmix 8 \
+        --samp_split \
+        --ps_seed=$ps_seed
 
-# done
+done
 
-# # spotless
-# for i in "${!ps_seeds[@]}"; do
-#     ps_seed=${ps_seeds[$i]}
+# spotless
+for i in "${!ps_seeds[@]}"; do
+    ps_seed=${ps_seeds[$i]}
     
-#     echo ps_seed: $ps_seed
-#     ./prep_data.py -s minmax \
-#         --dset mouse_cortex \
-#         --st_id spotless_mouse_cortex \
-#         --sc_id GSE115746 \
-#         --nmarkers 40 \
-#         --nmix 5 \
-#         --samp_split \
-#         --ps_seed=$ps_seed
+    echo ps_seed: $ps_seed
+    ./prep_data.py -s minmax \
+        --dset mouse_cortex \
+        --st_id spotless_mouse_cortex \
+        --sc_id GSE115746 \
+        --nmarkers 40 \
+        --nmix 5 \
+        --samp_split \
+        --ps_seed=$ps_seed
 
-# done
+done
 
-# pdac
+pdac
 for i in "${!ps_seeds[@]}"; do
     ps_seed=${ps_seeds[$i]}
     
