f758dc6a2418a69fcb622f72a5b6a952d03964c1
William Ma <mawilliam.v20@gmail.com>
2023-07-13 20:43:08 -0400

    finished dlpfc pipeline

--- batch_scripts/submit/submit_dlpfc.sh
+++ batch_scripts/submit/submit_dlpfc.sh
@@ -9,17 +9,19 @@ mkdir -p logs/DANN/generated_dlpfc
 
 GENCONFIG_JOBID=$(sbatch "batch_scripts/sbatch-gen-configs-dlpfc.sh" | tr -dc '0-9')
 sleep 5
-PREP_JOBID=$(sbatch "batch_scripts/sbatch-prep.sh" | tr -dc '0-9')
+PREP_JOBID=$(sbatch "batch_scripts/sbatch-prep-dlpfc.sh" | tr -dc '0-9')
 sleep 5
 
-sbatch -d afterok:$GENCONFIG_JOBID,afterok:$PREP_JOBID "batch_scripts/ADDA/sbatch-adda-ht-dlpfc.sh"
+ADDA_JOBID=$(sbatch -d afterok:$GENCONFIG_JOBID,afterok:$PREP_JOBID "batch_scripts/ADDA/sbatch-adda-ht-dlpfc.sh" | tr -dc '0-9')
 sleep 5
 
-sbatch -d afterok:$GENCONFIG_JOBID,afterok:$PREP_JOBID "batch_scripts/CellDART/sbatch-celldart-ht-dlpfc.sh"
+CELLDART_JOBID=$(sbatch -d afterok:$GENCONFIG_JOBID,afterok:$PREP_JOBID "batch_scripts/CellDART/sbatch-celldart-ht-dlpfc.sh" | tr -dc '0-9')
 sleep 5
 
-sbatch -d afterok:$GENCONFIG_JOBID,afterok:$PREP_JOBID "batch_scripts/CORAL/sbatch-coral-ht-dlpfc.sh"
+CORAL_JOBID=$(sbatch -d afterok:$GENCONFIG_JOBID,afterok:$PREP_JOBID "batch_scripts/CORAL/sbatch-coral-ht-dlpfc.sh" | tr -dc '0-9')
 sleep 5
 
-sbatch -d afterok:$GENCONFIG_JOBID,afterok:$PREP_JOBID "batch_scripts/DANN/sbatch-dann-ht-dlpfc.sh"
+DANN_JOBID=$(sbatch -d afterok:$GENCONFIG_JOBID,afterok:$PREP_JOBID "batch_scripts/DANN/sbatch-dann-ht-dlpfc.sh" | tr -dc '0-9')
 sleep 5
+
+sbatch -d afterok:$ADDA_JOBID,afterok:$CELLDART_JOBID,afterok:$CORAL_JOBID,afterok:$DANN_JOBID "batch_scripts/sbatch-final_hyperval-dlpfc.sh"
